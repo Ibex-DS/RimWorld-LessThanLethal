64f3a5ab44058e36d11cebd17d03f900a6afbb73
J4Wx <Jamie.Wood@TheIbex.net>
2017-09-26 16:26:50 +0100

    Updated for A17

--- Source/LessThanLethal/LessThanLethal/newCheckState.cs
+++ Source/LessThanLethal/LessThanLethal/newCheckState.cs
@@ -8,7 +8,8 @@ using System.Reflection;
 using System.Linq;
 using Verse.AI;
 using HugsLib;
-using HugsLib.Source.Detour;
+using RimWorld.Planet;
+using Harmony;
 
 namespace LTL
 {
@@ -30,53 +31,54 @@ namespace LTL
 			likelihood_nonlethal = Settings.GetHandle<int>("nonLethal", "Less Than Lethal Lethality", "How lethal should less than lethal weapons be? Determines how often people will be killed upon being incapacitated by less than lethal means.", 0);
 		}
 	}
-	
 		
-	
+	[HarmonyPatch(typeof(Verse.Pawn_HealthTracker), "CheckForStateChange")]
 	public static class stateChange {
 		internal static BindingFlags BF = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static;
-		
-		[DetourMethod(typeof(Verse.Pawn_HealthTracker), "CheckForStateChange")]
-		internal static void _StateCheck (this Verse.Pawn_HealthTracker self, Verse.DamageInfo? dinfo, Verse.Hediff hediff) {
-			
+		[HarmonyPrefix]
+		internal static bool _StateCheck (this Verse.Pawn_HealthTracker __instance, Verse.DamageInfo? dinfo, Verse.Hediff hediff) {
 			MethodInfo _shouldBeDead = typeof(Verse.Pawn_HealthTracker).GetMethod("ShouldBeDead", BF);
 			FieldInfo _pawn = typeof(Verse.Pawn_HealthTracker).GetField("pawn", BF);
 			MethodInfo _shouldBeDowned = typeof(Verse.Pawn_HealthTracker).GetMethod("ShouldBeDowned", BF);
 			MethodInfo _makeDowned = typeof(Verse.Pawn_HealthTracker).GetMethod("MakeDowned", BF);
 			MethodInfo _makeUndowned = typeof(Verse.Pawn_HealthTracker).GetMethod("MakeUndowned", BF);
 			
-			bool shouldBeDead = (bool)_shouldBeDead.Invoke(self, new object[]{});
-			bool shouldBeDowned = (bool)_shouldBeDowned.Invoke(self, new object[]{});
+			bool shouldBeDead = (bool)_shouldBeDead.Invoke(__instance, new object[]{});
+			bool shouldBeDowned = (bool)_shouldBeDowned.Invoke(__instance, new object[]{});
 			bool isNotLethalDam = ((dinfo != null) ? (dinfo.ToString().Contains('=') ? dinfo.ToString().Split('=')[1].Split(',')[0].StartsWith("LTL_") : false) : false);
 			bool isNotLethalDis = ((hediff != null) ? hediff.ToString().StartsWith("(LTL_") : false);
 			
-			Verse.Pawn pawn = (Verse.Pawn)_pawn.GetValue(self);
-			if (!self.Dead) {
+			Verse.Pawn pawn = (Verse.Pawn)_pawn.GetValue(__instance);
+			if (!__instance.Dead) {
 				if (shouldBeDead) {
 					if (!pawn.Destroyed) {
-						self.Kill(dinfo, hediff);
+						bool flag = PawnUtility.ShouldSendNotificationAbout(pawn);
+						Caravan caravan = pawn.GetCaravan();
+						pawn.Kill(dinfo);
+						if (flag) {
+							__instance.NotifyPlayerOfKilled(dinfo, hediff, caravan);
+						}
 					}
-					return;
+					return false;
 				}
-				if (!self.Downed) {
+				if (!__instance.Downed) {
 					if (shouldBeDowned) {
 						float num = (isNotLethalDam || isNotLethalDis) ? ((float)LTL.likelihood_nonlethal/(float)100) : ((!pawn.RaceProps.Animal) ? ((float)LTL.likelihood_lethalHuman/(float)100) : ((float)LTL.likelihood_lethalAnimal/(float)10));
-						if (!self.forceIncap && (pawn.Faction == null || !pawn.Faction.IsPlayer) && !pawn.IsPrisonerOfColony && pawn.RaceProps.IsFlesh && Verse.Rand.Value < num) {
-							self.Kill(dinfo, null);
-							return;
+						if (!__instance.forceIncap && (pawn.Faction == null || !pawn.Faction.IsPlayer) && !pawn.IsPrisonerOfColony && pawn.RaceProps.IsFlesh && Verse.Rand.Value < num) {
+							pawn.Kill(dinfo);
+							return false;
 						}
-						self.forceIncap = false;
-						_makeDowned.Invoke(self, new object[] {dinfo, hediff});
-						return;
-					} else if (!self.capacities.CapableOf(PawnCapacityDefOf.Manipulation)) {
+						__instance.forceIncap = false;
+						_makeDowned.Invoke(__instance, new object[] {dinfo, hediff});
+						return false;
+					} else if (!__instance.capacities.CapableOf(PawnCapacityDefOf.Manipulation)) {
 						if (pawn.carryTracker != null && pawn.carryTracker.CarriedThing != null && pawn.jobs != null && pawn.CurJob != null) {
 							pawn.jobs.EndCurrentJob(JobCondition.InterruptForced, true);
 						}
 						if (pawn.equipment != null && pawn.equipment.Primary != null) {
 							if (pawn.InContainerEnclosed) {
-								Verse.ThingWithComps thingWithComps;
-								pawn.equipment.TryTransferEquipmentToContainer(pawn.equipment.Primary, pawn.holdingContainer, out thingWithComps);
-							} else if (pawn.Spawned) {
+								pawn.equipment.TryTransferEquipmentToContainer(pawn.equipment.Primary, pawn.holdingOwner);
+							} else if (pawn.SpawnedOrAnyParentSpawned) {
 								Verse.ThingWithComps thingWithComps;
 								pawn.equipment.TryDropEquipment(pawn.equipment.Primary, out thingWithComps, pawn.Position, true);
 							} else {
@@ -85,10 +87,11 @@ namespace LTL
 						}
 					}
 				} else if (!shouldBeDowned) {
-					_makeUndowned.Invoke(self, new object[] {});
-					return;
+					_makeUndowned.Invoke(__instance, new object[] {});
+					return false;
 				}
 			}
+			return false;
 		}
 	}
 }
